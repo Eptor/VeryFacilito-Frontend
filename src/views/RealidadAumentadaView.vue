669c53a3eb25f420402ad1143ce4d92574ac240b
Hector Espinoza <hector_espinoza02@hotmail.com>
2024-04-15 18:38:09 -0700

    Estamos dentro

--- src/views/RealidadAumentadaView.vue
+++ src/views/RealidadAumentadaView.vue
@@ -1,40 +1,22 @@
 <template>
-    <div>
-        <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
-            <a-marker preset="hiro" @markerFound="onMarkerFound">
-                <a-box position="0 0.5 0" material="color: red;"></a-box>
-            </a-marker>
-            <a-entity camera></a-entity>
-        </a-scene>
-    </div>
+    <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
+        <a-box position="0 0.5 0" material="color: yellow;"></a-box>
+        <a-marker-camera preset="hiro"></a-marker-camera>
+    </a-scene>
 </template>
 
 <script>
 export default {
-    name: 'RealidadAumentadaView',
-    mounted() {
-        this.startCamera()
-    },
+    name: "App",
     methods: {
-        async startCamera() {
-            try {
-                const constraints = {
-                    video: {
-                        facingMode: 'user', // use the front camera
-                        width: { ideal: 4096 },
-                        height: { ideal: 2160 }
-                    }
-                }
-                const stream = await navigator.mediaDevices.getUserMedia(constraints)
-                this.$refs.video.srcObject = stream
-                this.$refs.video.play()
-            } catch (error) {
-                console.error('Error accessing media devices.', error)
-            }
-        },
         onMarkerFound() {
-            console.log('Patrón detectado');
+            console.log("Marcador 'hiro' encontrado");
+        },
+        onMarkerLost() {
+            console.log("Marcador 'hiro' perdido");
         }
     }
-}
+};
 </script>
+
+<style lang="scss"></style>
